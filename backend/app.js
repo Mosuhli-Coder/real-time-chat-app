4a612021137214495f15ce9d92b9ff9d6dc7032c
Teboho Mosuhli <drmosuhli@gmail.com>
2024-02-08 21:23:13 +0200

    Deployment commit

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,7 @@ const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -22,6 +24,13 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectToMongoDB();
